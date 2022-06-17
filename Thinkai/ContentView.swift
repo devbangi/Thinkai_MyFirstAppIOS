6f15193fef7490834806ef5096c913a872d75515
devbangi <angela.bureaca@gmail.com>
2022-06-17 02:21:12 -0700

    Initial Commit

---
+++ Thinkai/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Thinkai
+//
+//  Created by Angi on 6/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Thinkai/ThinkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkaiApp.swift
+//  Thinkai
+//
+//  Created by Angi on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ThinkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
