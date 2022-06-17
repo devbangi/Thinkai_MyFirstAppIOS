204e8f99a939da65292f5392de9edc2f77892d00
devbangi <angela.bureaca@gmail.com>
2022-06-17 05:06:50 -0700

    Small update

--- Thinkai/ContentView.swift
+++ Thinkai/ContentView.swift
@@ -15,17 +15,12 @@ struct ContentView: View {
                 Image("pete").resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0)).frame(height: 100.0).position(x: 205.9, y: 31.0).aspectRatio(contentMode: .fit)
                 Spacer()
                 Group{
-                Button("Sing in with Facebook") {
-                    
+                    Button("Sing in with Facebook") {}
+                    Spacer()
+                    Button("Sing in with Google") {}
+                    Spacer()
+                    Button("Sing in with Email") {}
                 }
-                Spacer()
-                Button("Sing in with Google") {
-            
-                }
-                Spacer()
-                Button("Sing in with Email") {
-                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                }}
                 Spacer()
                 Image("background").resizable().aspectRatio(contentMode: .fit)
                 Label("", systemImage: "bolt.fill").padding().frame(width: 550, height: 20).background(Color.mint).lineLimit(300).font(.custom("Times New Roman", size: 20))
