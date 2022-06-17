326f13ec95d03fa7d29178f179f68d6ed4acdf6c
devbangi <angela.bureaca@gmail.com>
2022-06-17 03:30:12 -0700

    Main window: interface without buttons access

--- Thinkai/ContentView.swift
+++ Thinkai/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Group{
+                Label("THINKAI", systemImage: "bolt.fill").frame(width: 550, height: 40).background(Color.mint).lineLimit(300).font(.custom("Times New Roman", size: 27)).position(x: 205.0, y: 0.0).frame(width: 422.0 , height: 31.0)
+                Image("pete").resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0)).frame(height: 100.0).position(x: 205.9, y: 31.0).aspectRatio(contentMode: .fit)
+                Spacer()
+                Group{
+                Button("Sing in with Facebook") {
+                    
+                }
+                Spacer()
+                Button("Sing in with Google") {
+            
+                }
+                Spacer()
+                Button("Sing in with Email") {
+                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }}
+                Spacer()
+                Image("background").resizable().aspectRatio(contentMode: .fit)
+                Label("", systemImage: "bolt.fill").padding().frame(width: 550, height: 20).background(Color.mint).lineLimit(300).font(.custom("Times New Roman", size: 20))
+                            }
+        }
     }
 }
 
