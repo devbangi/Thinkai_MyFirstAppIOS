9de7dec1b41f720a04054f0dbd1aea01e01ed6a2
devbangi <96576249+devbangi@users.noreply.github.com>
2022-06-22 03:02:51 -0700

    Add files via upload

---
+++ Thinkai_version2/Thinkai_version2/FirstWindow2ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  FirstWindow2ViewController.swift
+//  Thinkai_version2
+//
+//  Created by Angi on 6/21/22.
+//
+
+import UIKit
+
+class FirstWindow2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        print("Sign in.")
+    }
+    
+    @IBAction func signInWithFbButtonTapped(_ sender: Any) {
+        print("Sign in with facebook.")
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Thinkai_version2/Thinkai_version2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Thinkai_version2
+//
+//  Created by Angi on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
